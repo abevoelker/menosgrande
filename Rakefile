3fd55a468e25a8bdb46f879d46e80623cd3d5aa5
Abe Voelker <abe@abevoelker.com>
2011-05-29 15:19:16 -0500

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,12 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+require 'rake'
+
+#Fix from http://stackoverflow.com/questions/5287121/undefined-method-task-using-rake-0-9-0-beta-4
+class Rails::Application
+  include Rake::DSL if defined?(Rake::DSL)
+end
+
+UrlShorten::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShorten::Application.config.secret_token = '90961e07c4a4cde17a2ba68eb7a1fbecc751ade0e15bfb0f1f73daf0b3126bd6fbd607421733a4ef259ee1c96ac6319ea6555f04a7d8f2ab411cf6475eda1ac1'

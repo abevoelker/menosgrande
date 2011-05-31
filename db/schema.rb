a0cc09bee035e781e2d4a77df65c91d5bbf06b22
Abe Voelker <abe@abevoelker.com>
2011-05-31 12:45:55 -0500

    Add index on key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110529203420) do
+ActiveRecord::Schema.define(:version => 20110531173735) do
 
   create_table "shorteners", :force => true do |t|
     t.string   "url"
@@ -18,5 +18,7 @@ ActiveRecord::Schema.define(:version => 20110529203420) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "shorteners", ["key"], :name => "index_shorteners_on_key", :unique => true
 
 end

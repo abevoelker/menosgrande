e4f05570800fb2a1e87b170d11ed5f7ca23c6505
Abe Voelker <abe@abevoelker.com>
2011-06-14 20:57:38 -0500

    Revert "Attempt #2 at removing www. prefix from hostname"
    This reverts commit 2a13818ca1b96a088236ec64f1ec46374a95c00d.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Rails::Initializer.run do |config|
-  config.middleware.use “NoWWW” if RAILS_ENV == ‘production’
-end
-
 # Initialize the rails application
 UrlShorten::Application.initialize!
